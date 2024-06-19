25e053ec77347d61e79856992185cc5c1a5212d5
Gabriel Wersebe <gwersebe1@gmail.com>
2024-06-19 17:22:11 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Personal Website
+This is a personal website that serves as an online version of my resume. It contains information about my education, work experience, skills, and projects.
+
+# Built With
+The website is built using HTML, CSS, and JavaScript, and is designed to be responsive and accessible on different devices and screen sizes. It uses popular frameworks like Bootstrap and Font Awesome for styling and icons, and jQuery for DOM manipulation. In addition, I use Firebase Cloud Functions and Nodemailer to send me form responces 
+
+# [Start Bootstrap - Resume](https://startbootstrap.com/theme/resume/)
+
+[Resume](https://startbootstrap.com/theme/resume/) is a resume and CV theme for [Bootstrap](https://getbootstrap.com/) created by [Start Bootstrap](https://startbootstrap.com/). This theme features a fixed sidebar with content sections to build a simple, yet elegant resume.
+
+
+## Copyright and License
+
+Copyright 2013-2022 Start Bootstrap LLC. Code released under the [MIT](https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE) license.
---
+++ src/js/scripts.js
@@ -0,0 +1,207 @@
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+// when form is sumbited send data to firebase function 
+// function sendEmail() {
+//     const name = document.getElementById('name').value;
+//     const email = document.getElementById('email').value;
+//     const message = document.getElementById('message').value;
+//     const data = {
+//       name,
+//       email,
+//       message
+//     };
+//     fetch(`https://us-central1-personal-website-bb395.cloudfunctions.net/sendEmail?email=${email}&name=${name}&message=${message}`, {
+//       method: 'POST',
+//       body: JSON.stringify(data),
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     }).then(res => {
+//       console.log('Request complete! response:', res);
+//     });
+//   }
+  
+//   const form = document.getElementById('contactForm');
+//   const button = document.getElementById('sendMessageButton');
+  
+//   button.addEventListener('sumbit', (e) => {
+//     e.preventDefault();
+//     sendEmail();
+//   });
+
+
+
+const button = document.querySelector('#sendMessageButton');
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  const email = document.querySelector('#email').value;
+  const name = document.querySelector('#name').value;
+  const message = document.querySelector('#message').value;
+  const url = `https://us-central1-personal-website-bb395.cloudfunctions.net/sendEmail?email=${email}&name=${name}&message=${message}`;
+  fetch(url)
+  .then(response => {
+    if (response.status === 200) {
+      console.log('Email sent!');
+      document.querySelector('#email').value = '';
+      document.querySelector('#name').value = '';
+      document.querySelector('#message').value = '';
+      window.alert('Email sent successfully!');
+    } else {
+      console.error('Error sending email:', response.status);
+      window.alert('Error sending email. Please try again later.');
+    }
+  })
+  .catch(error => {
+    console.error('Error sending email:', error);
+    window.alert('Error sending email. Please try again later.');
+  });
+});
+
+
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// const button = document.querySelector('#sendMessageButton');
+
+// button.addEventListener('click', (event) => {
+//   event.preventDefault();
+//   const email = document.querySelector('#email').value;
+//   const name = document.querySelector('#name').value;
+//   const message = document.querySelector('#message').value;
+//   const url = `https://us-central1-personal-website-bb395.cloudfunctions.net/sendEmail?email=${email}&name=${name}&message=${message}`;
+//   fetch(url)
+//     .then(response => {
+//       if (response.ok) {
+//         console.log('Email sent!');
+
+//         // clear form
+//         document.querySelector('#email').value = '';
+//         document.querySelector('#name').value = '';
+//         document.querySelector('#message').value = '';
+
+//       } else {
+//         console.error('Error sending email:', response.status);
+//       }
+//     })
+//     .catch(error => {
+//       console.error('Error sending email:', error);
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.querySelector('contactForm"');
+
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+//   const name = form.elements['name'].value;
+//   const email = form.elements['email'].value;
+//   const message = form.elements['message'].value;
+
+//   const sendEmail = firebase.functions().httpsCallable('sendEmail');
+//   sendEmail({ name, email, message })
+//     .then((result) => {
+//       console.log(result);
+//       alert('Your message was sent successfully!');
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       alert('There was an error sending your message. Please try again later.');
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   const form = document.getElementById('contactForm');
+
+//   form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     sendEmail();
+//   });
+  
+
+
+//   function sendEmail() {
+//     const name = document.getElementById('name').value;
+//     const email = document.getElementById('email').value;
+//     const message = document.getElementById('message').value;
+//     const data = {
+//       name,
+//       email,
+//       message
+//     };
+//     fetch('https://us-central1-personal-website-bb395.cloudfunctions.net/sendEmail', {
+//       method: 'POST',
+//       body: JSON.stringify(data),
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     }).then(res => {
+//       console.log('Request complete! response:', res);
+//     });
+//   }
+  
+
+  
+// get teh current year and add it to the footer
+// const year = new Date().getFullYear();
+// document.querySelector('#currentYear').textContent = year;
+
+
+
+
+
+
+
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
